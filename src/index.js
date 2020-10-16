3527fc49e689226807214d3bebcde909850e7df6
pam-param <tamarova.irina@gmail.com>
2020-10-16 20:27:49 +0300

    feat: add brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsMap = new Map(bracketsConfig.map(item => [item[1], item[0]]));
+  let stack = [];
+  const values = Array.from(bracketsMap.values());
+
+  for (let char of str) {
+    if (values.includes(char)) {
+      if (bracketsMap.get(char) === char && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (bracketsMap.has(char)) {
+        if (bracketsMap.get(char) !== stack.pop())
+        {
+            return false;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
